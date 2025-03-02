4b86158fe47c8769e7f018498d2f976360195183
YQSoverflow <connoryangsingapore@gmail.com>
2025-03-02 20:46:59 +0800

    update aggregation

--- src/config.py
+++ src/config.py
@@ -1,4 +1,5 @@
 TRAIN_FILE = '../data/Crimes_-_2001_to_Present.csv'
-TRAIN_FILE_CLEAN = '../data/chicago_clean.csv'
+TRAIN_FILE_CLEAN_h5 = '../data/chicago_clean_final.h5'
+TRAIN_FILE_CLEAN = ['../data/chicago_clean_final_arrest.csv', '../data/chicago_clean_final_not_arrest.csv']
 SEED = 42
 VISUALIZATION = 0
