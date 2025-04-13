490526b92f5f4a8a5bbc3b7295ff34a8443ac0c2
YQSoverflow <connoryangsingapore@gmail.com>
2025-04-14 00:51:39 +0800

    rewrite spiral feature

--- src/config.py
+++ src/config.py
@@ -2,5 +2,6 @@ TRAIN_FILE = '../data/Crimes_-_2001_to_Present.csv'
 TRAIN_FILE_CLEAN_h5 = '../data/chicago_clean_final.h5'
 TRAIN_FILE_CLEAN = ['../data/chicago_clean_final_arrest.csv', '../data/chicago_clean_final_not_arrest.csv']
 TRAIN_FILE_CLEAN2 = '../data/chicago_clean_final.csv'
+TRAIN_FILE_FEATURE = '../data/chicago_feature.csv'
 SEED = 42
 VISUALIZATION = 0
