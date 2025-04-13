dc553ea072f44755ecd67fb9de38f1ec379ea0ff
YQSoverflow <connoryangsingapore@gmail.com>
2025-04-14 00:08:58 +0800

    remove split, fix preprocessing, add data load to feature_eng

--- src/config.py
+++ src/config.py
@@ -1,5 +1,6 @@
 TRAIN_FILE = '../data/Crimes_-_2001_to_Present.csv'
 TRAIN_FILE_CLEAN_h5 = '../data/chicago_clean_final.h5'
 TRAIN_FILE_CLEAN = ['../data/chicago_clean_final_arrest.csv', '../data/chicago_clean_final_not_arrest.csv']
+TRAIN_FILE_CLEAN2 = '../data/chicago_clean_final.csv'
 SEED = 42
 VISUALIZATION = 0
