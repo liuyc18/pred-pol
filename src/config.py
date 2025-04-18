31a3224d6f48290213f7f730c18a7276dc7f4cea
YQSoverflow <connoryangsingapore@gmail.com>
2025-04-18 17:09:07 +0800

    redo feature engineering, fix code, delete old files

--- src/config.py
+++ src/config.py
@@ -2,6 +2,6 @@ TRAIN_FILE = '../data/Crimes_-_2001_to_Present.csv'
 TRAIN_FILE_CLEAN_h5 = '../data/chicago_clean_final.h5'
 TRAIN_FILE_CLEAN = ['../data/chicago_clean_final_arrest.csv', '../data/chicago_clean_final_not_arrest.csv']
 TRAIN_FILE_CLEAN2 = '../data/chicago_clean_final.csv'
-TRAIN_FILE_FEATURE = '../data/chicago_feature.csv'
 SEED = 42
 VISUALIZATION = 0
+WINDOW_SECS = [6, 24, 24*7, 24*30, 24*30*3, 24*365]
